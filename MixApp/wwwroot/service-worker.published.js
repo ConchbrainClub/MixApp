3f36ff858325f223b9d2ad9bbdc7c62d8e08c9e7
lixinyang123 <lixinyangemil@outlook.com>
2023-07-26 04:22:55 +0800

    do not delete unused caches

--- MixApp/wwwroot/service-worker.published.js
+++ MixApp/wwwroot/service-worker.published.js
@@ -29,11 +29,11 @@ async function onInstall(event) {
 async function onActivate(event) {
     console.info('Service worker: Activate');
 
-    // Delete unused caches
-    const cacheKeys = await caches.keys();
-    await Promise.all(cacheKeys
-        .filter(key => key.startsWith(cacheNamePrefix) && key !== cacheName)
-        .map(key => caches.delete(key)));
+    // // Delete unused caches
+    // const cacheKeys = await caches.keys();
+    // await Promise.all(cacheKeys
+    //     .filter(key => key.startsWith(cacheNamePrefix) && key !== cacheName)
+    //     .map(key => caches.delete(key)));
 }
 
 async function onFetch(event) {
