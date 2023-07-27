37eb2b1ec10a7a99d5078ea89c2fac08eb8ff205
lixinyang123 <lixinyangemil@outlook.com>
2023-07-28 01:39:25 +0800

    add locale manager

---
+++ MixApp/Services/LocaleManager.cs
@@ -0,0 +1,45 @@
+using System.Net.Http.Json;
+using System.Text.Json;
+using MixApp.Models;
+
+namespace MixApp.Services
+{
+    public class LocaleManager
+    {
+        private HttpClient httpClient;
+
+        private string locale = string.Empty;
+
+        public event Action? OnLoaded;
+
+        public Dictionary<string, string> Scripts { get; set; }
+
+        public string Locale
+        {
+            get => locale;
+            set
+            {
+                locale = value;
+                LoadScript();
+            }
+        }
+
+        public LocaleManager(string baseAddress)
+        {
+            Scripts = new();
+
+            httpClient = new HttpClient()
+            {
+                BaseAddress = new Uri(baseAddress)
+            };
+
+            Locale = "zh-CN";
+        }
+
+        public async void LoadScript()
+        {
+            Scripts = await httpClient.GetFromJsonAsync<Dictionary<string, string>>($"/locale/{Locale}.json") ?? new();
+            OnLoaded?.Invoke();
+        }
+    }
+}
