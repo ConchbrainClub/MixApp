3ae70c50ca862a7c0f920958bf8cac7e94e83e2e
dangzihei111 <dangzihei111@outlook.com>
2023-07-28 10:49:40 +0800

    update sideBar

--- MixApp/Components/SideBar.razor.cs
+++ MixApp/Components/SideBar.razor.cs
@@ -1,9 +1,32 @@
 using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System.Threading.Tasks;
 
 namespace MixApp.Components
 {
     public partial class SideBarBase : ComponentBase
     {
-        public string ActivePage { get; set; } = "Home";
+        [Inject]
+        public NavigationManager Navigation { get; set; }
+
+        [Inject]
+        public IJSRuntime JSRuntime { get; set; }
+
+        public string ActivePage { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            var uri = new Uri(Navigation.Uri);
+            string pageName = uri.Segments.Last();
+            await JSRuntime.InvokeVoidAsync("console.log",pageName);
+            // if (pageName == "/")
+            // {
+            //     ActivePage = "Home";
+            // }
+            // else
+            // {
+                ActivePage = pageName;
+            
+        }
     }
 }
