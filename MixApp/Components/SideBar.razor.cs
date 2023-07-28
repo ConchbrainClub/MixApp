d15c1f4a47fc8d2e0613351ece32d2584bf3e96c
lixinyang123 <lixinyangemil@outlook.com>
2023-07-29 00:11:47 +0800

    fixed sidebar error

--- MixApp/Components/SideBar.razor.cs
+++ MixApp/Components/SideBar.razor.cs
@@ -1,9 +1,73 @@
 using Microsoft.AspNetCore.Components;
+using Microsoft.Fast.Components.FluentUI;
 
 namespace MixApp.Components
 {
     public partial class SideBarBase : ComponentBase
     {
-        public string ActivePage { get; set; } = "Home";
+        [Inject]
+        public NavigationManager Navigation { get; set; } = default!;
+
+        public List<RouteInfo> Top { get; set; }
+
+        public List<RouteInfo> Bottom { get; set; }
+
+        public string CurrentHref { get; set; }
+
+        public SideBarBase()
+        {
+            Top = new()
+            {
+                new()
+                {
+                    Href = "/",
+                    IconFilled = new Icons.Filled.Size24.Home(),
+                    IconRegular = new Icons.Regular.Size24.Home(),
+                    Name = "Home"
+                },
+                new()
+                {
+                    Href = "/softwares",
+                    IconFilled = new Icons.Filled.Size24.Apps(),
+                    IconRegular = new Icons.Regular.Size24.Apps(),
+                    Name = "Apps"
+                },
+                new()
+                {
+                    Href = "/counter",
+                    IconFilled = new Icons.Filled.Size24.Library(),
+                    IconRegular = new Icons.Regular.Size24.Library(),
+                    Name = "Library"
+                }
+            };
+
+            Bottom = new()
+            {
+                new()
+                {
+                    Href = "/setting",
+                    IconFilled = new Icons.Filled.Size24.Settings(),
+                    IconRegular = new Icons.Regular.Size24.Settings(),
+                    Name = "Setting"
+                },
+                new()
+                {
+                    Href = "/help",
+                    IconFilled = new Icons.Filled.Size24.ChatHelp(),
+                    IconRegular = new Icons.Regular.Size24.ChatHelp(),
+                    Name = "Help"
+                }
+            };
+
+            CurrentHref = "/";
+        }
+
+        protected override void OnInitialized()
+        {
+            RouteInfo routeInfo = Top.Concat(Bottom)
+                .SingleOrDefault(i => new Uri(Navigation!.Uri).AbsolutePath == i.Href) ?? Top.First();
+
+            CurrentHref = routeInfo.Href;
+        }
     }
 }
