ba6415db17677afc9aa35815b0a7ab294d4946c7
lixinyang123 <lixinyangemil@outlook.com>
2023-08-03 21:37:14 +0800

    complete setting page

--- MixApp/Pages/Setting.razor.cs
+++ MixApp/Pages/Setting.razor.cs
@@ -1,9 +1,11 @@
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components;
 
 namespace MixApp.Pages
 {
     public partial class SettingBase : ComponentBase
     {
-        
+        [Inject]
+        ILocalStorageService LocalStorage { get; set; } = default!;
     }
 }
