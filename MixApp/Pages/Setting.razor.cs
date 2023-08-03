cf2e92e2127fc38c016723ea15ec3706ccd07ac3
lixinyang123 <lixinyangemil@outlook.com>
2023-08-03 21:10:58 +0800

    complete setting page

--- MixApp/Pages/Setting.razor.cs
+++ MixApp/Pages/Setting.razor.cs
@@ -4,11 +4,6 @@ namespace MixApp.Pages
 {
     public partial class SettingBase : ComponentBase
     {
-        public int currentCount = 0;
-
-        public void IncrementCount()
-        {
-            currentCount++;
-        }
+        
     }
 }
