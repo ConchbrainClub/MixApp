02e0d02c1f869a049b00ef1ef00b6026bd6d369b
lixinyang123 <lixinyangemil@outlook.com>
2023-08-14 00:18:13 +0800

    add page title

--- MixApp/Services/GlobalEvent.cs
+++ MixApp/Services/GlobalEvent.cs
@@ -8,6 +8,8 @@ namespace MixApp.Services
 
         public event Action<string>? OnChangeTheme;
 
+        public List<Software> Softwares { get; set; } = new();
+
         public void OpenSoftware(Software software)
         {
             OnOpenSoftware?.Invoke(software);
@@ -16,6 +18,11 @@ namespace MixApp.Services
         public void ChangeTheme(string color)
         {
             OnChangeTheme?.Invoke(color);
+        }
+
+        public void Add2DownloadList(Software software) 
+        {
+            Softwares.Add(software);
         }
     }
 }
