4c222973936b5034fdd75924e2c66b4b0c0a26d3
lixinyang123 <lixinyangemil@outlook.com>
2023-08-26 22:01:39 +0800

    support chromeless

--- MixApp.Client/Program.cs
+++ MixApp.Client/Program.cs
@@ -1,29 +1,29 @@
 ﻿﻿using PhotinoNET;
 using System.Drawing;
 
-namespace MixApp.Client
+namespace MixApp.Client;
+
+class Program
 {
-    class Program
+    [STAThread]
+    static void Main(string[] _)
     {
-        [STAThread]
-        static void Main(string[] _)
-        {
-            string windowTitle = "MixStore";
+        string windowTitle = "MixStore";
 
-            var window = new PhotinoWindow()
-                .SetTitle(windowTitle)
-                .SetUseOsDefaultSize(false)
-                .SetSize(new Size(1400, 950))
-                .Center()
-                .RegisterWebMessageReceivedHandler((object? sender, string message) =>
-                {
-                    PhotinoWindow? window = sender as PhotinoWindow;
-                    string response = $"Received message: \"{message}\"";
-                    window?.SendWebMessage(response);
-                })
-                .Load(new Uri("https://mixstore.conchbrain.club/"));
+        var window = new PhotinoWindow()
+            .SetTitle(windowTitle)
+            .SetUseOsDefaultSize(false)
+            .SetSize(new Size(1400, 930))
+            // .SetChromeless(true)
+            .Center()
+            .RegisterWebMessageReceivedHandler((object? sender, string message) =>
+            {
+                PhotinoWindow? window = sender as PhotinoWindow;
+                string response = $"Received message: \"{message}\"";
+                window?.SendWebMessage(response);
+            })
+            .Load(new Uri("https://mixstore.conchbrain.club/"));
 
-            window.WaitForClose();
-        }
+        window.WaitForClose();
     }
 }
