ba58fb710eb9e349dc4ea2f249f3d908026f6bc4
lixinyang123 <lixinyangemil@outlook.com>
2023-10-22 06:27:24 +0800

    fixed notify error

---
+++ MixApp.Shared/Components/RequestPermission.razor.cs
@@ -0,0 +1,48 @@
+using Append.Blazor.Notifications;
+using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Fast.Components.FluentUI;
+
+namespace MixApp.Shared.Components
+{
+    public partial class RequestPermissionBase : ComponentBase
+    {
+        [Inject]
+        ILocalStorageService LocalStorage { get; set; } = default!;
+
+        [Inject]
+        INotificationService NotificationService { get; set; } = default!;
+
+        private bool showDialog = false;
+
+        public bool ShowDialog
+        {
+            get => showDialog;
+            set
+            {
+                showDialog = value;
+                StateHasChanged();
+            }
+        }
+
+        protected async override Task OnInitializedAsync()
+        {
+            if (!await NotificationService.IsSupportedByBrowserAsync()) return;
+            if (!string.IsNullOrEmpty(await LocalStorage.GetItemAsStringAsync("NotificationPermission"))) return;
+
+            ShowDialog = true;
+        }
+
+        public async void RequestPermission()
+        {
+            PermissionType permission = await NotificationService.RequestPermissionAsync();
+            if (permission == PermissionType.Granted)
+            {
+                _ = NotificationService.CreateAsync("通知测试", "成功啦", "favicon.png").AsTask();
+            }
+
+            await LocalStorage.SetItemAsStringAsync("NotificationPermission", permission.ToString());
+            ShowDialog = false;
+        }
+    }
+}
--- MixApp.Shared/Shared/MainLayout.razor.cs
+++ MixApp.Shared/Shared/MainLayout.razor.cs
@@ -1,7 +1,5 @@
-using Append.Blazor.Notifications;
 using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components;
-using Microsoft.Fast.Components.FluentUI;
 using MixApp.Shared.Models;
 using MixApp.Shared.Services;
 
@@ -9,18 +7,11 @@ namespace MixApp.Shared.Shared
 {
     public partial class MainLayoutBase : LayoutComponentBase 
     {
-
         [Inject]
         ILocalStorageService LocalStorage { get; set; } = default!;
 
         [Inject]
         private GlobalEvent GlobalEvent { get; set; } = default!;
-
-        [Inject]
-        private IDialogService DialogService { get; set; } = default!;
-
-        [Inject]
-        private INotificationService NotificationService { get; set; } = default!;
 
         public bool Expanded { get; set; } = false;
 
@@ -51,38 +42,6 @@ namespace MixApp.Shared.Shared
             {
                 Color = color;
             }
-
-            CheckNotifyPermission();
-        }
-
-        private async void CheckNotifyPermission()
-        {
-            if(!await NotificationService.IsSupportedByBrowserAsync()) return;
-
-            if (await LocalStorage.GetItemAsStringAsync("NotificationPermission") == "Granted") return;
-
-            IDialogReference dialog = await DialogService.ShowMessageBoxAsync(new DialogParameters<MessageBoxContent>()
-            {
-                Content = new()
-                {
-                    Title = "获得更好的使用体验",
-                    MarkupMessage = new MarkupString
-                    (
-                        """
-                        <i>MixStore 推荐您开启以下权限</i>
-                        <br/><br/>
-                        允许 <u><strong>通知</strong></u> 权限来提醒您软件的下载状态
-                        """
-                    )
-                }
-            });
-
-            if ((await dialog.Result).Cancelled) return;
-
-            PermissionType permission = await NotificationService.RequestPermissionAsync();
-            if (permission != PermissionType.Granted) return;
-
-            await LocalStorage.SetItemAsStringAsync("NotificationPermission", permission.ToString());
         }
     }
 }
