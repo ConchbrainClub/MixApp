a42c038199768519bd62a07f8d8db3612b480879
lixinyang123 <lixinyangemil@outlook.com>
2023-11-29 01:48:38 +0800

    fixed feedback err

--- MixApp.Shared/Pages/Help.razor.cs
+++ MixApp.Shared/Pages/Help.razor.cs
@@ -1,52 +1,38 @@
 using System.ComponentModel.DataAnnotations;
-using Blazored.LocalStorage;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using Microsoft.AspNetCore.Components;
 
 namespace MixApp.Shared.Pages
 {
-  public class HelpBase : ComponentBase
-  {
-    [Inject]
-    ILocalStorageService LocalStorage { get; set; } = default!;
-    [Inject]
-    public HttpClient HttpClient { get; set; } = new HttpClient();
-
-    public bool submitLoading { get; set; } = false;
-
-    [Required(ErrorMessage = "请输入联系方式")]
-    public string contact { get; set; } = "1790120845@qq.com";
-
-    [Required(ErrorMessage = "请输入联系方式")]
-    public string content { get; set; } = "测试提交";
-
-    public void SubmitFeedback()
+    public record Feedback
     {
-      if (contact.Length == 0 || content.Length == 0)
-      {
-        return;
-      }
-      submitLoading = true;
-      var postContent = new MultipartFormDataContent();
-      // postContent.Headers.Add("ContentType", $"multipart/form-data");
-      postContent.Headers.Add("ContentType", "application/json");
-      postContent.Add(new StringContent(contact), "contact");
-      postContent.Add(new StringContent(content), "content");
-      string result = "";
-      try
-      {
+        [Required]
+        [JsonPropertyName("contact")]
+        public string Contact { get; set; } = string.Empty;
 
-        HttpResponseMessage response = HttpClient.PostAsync("/identifier", postContent).Result;
-        // HttpResponseMessage response = HttpClient.PostAsync("/v1/feedback", postContent).Result;
-        result = response.Content.ReadAsStringAsync().Result;
-        Console.WriteLine(result);
-        submitLoading = false;
-      }
-      catch (System.Exception)
-      {
+        [Required]
+        [JsonPropertyName("content")]
+        public string Content { get; set; } = string.Empty;
+    };
+    
+    public class HelpBase : ComponentBase
+    {
+        [Inject]
+        public HttpClient HttpClient { get; set; } = new HttpClient();
 
-        submitLoading = false;
-        throw;
-      }
+        public bool SubmitLoading { get; set; } = false;
+
+        public Feedback Feedback { get; set; } = new();
+
+        public void SubmitFeedback()
+        {
+            if (!Validator.TryValidateObject(Feedback, new ValidationContext(Feedback), null)) return;
+
+            SubmitLoading = true;
+            StringContent content = new(JsonSerializer.Serialize(Feedback));
+            _ = HttpClient.PostAsync("/v1/feedback", content);
+            SubmitLoading = false;
+        }
     }
-  }
 }
