8a50607e9fef2a7e8b8fcf0b10f01e0a0b434b75
lixinyang123 <lixinyangemil@outlook.com>
2024-01-19 13:48:19 +0800

    update to net 801

--- .build.sh
+++ .build.sh
@@ -1,9 +1,9 @@
 npm install
-mv ./node_modules/ ./MixApp.Shared/wwwroot/lib
+mv ./node_modules/ ./MixApp.Web/wwwroot/lib
 
 wget https://dotnet.microsoft.com/download/dotnet/scripts/v1/dotnet-install.sh
 chmod +x ./dotnet-install.sh
-./dotnet-install.sh -v 8.0.100-rc.2.23502.2 --install-dir ~/cli
+./dotnet-install.sh -v 8.0.101 --install-dir ~/cli
 
 ~/cli/dotnet workload restore
 ~/cli/dotnet restore
