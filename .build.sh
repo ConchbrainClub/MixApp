2ca48d802c6f8f58a8a3af7368c974e3771045b9
lixinyang123 <lixinyangemil@outlook.com>
2024-06-20 01:56:47 +0800

    upgrade dependences

--- .build.sh
+++ .build.sh
@@ -3,7 +3,7 @@ mv ./node_modules/ ./MixApp.Web/wwwroot/lib
 
 wget https://dotnet.microsoft.com/download/dotnet/scripts/v1/dotnet-install.sh
 chmod +x ./dotnet-install.sh
-./dotnet-install.sh -v 8.0.301 --install-dir ~/cli
+./dotnet-install.sh -v 8.0.302 --install-dir ~/cli
 
 ~/cli/dotnet workload restore
 ~/cli/dotnet restore
