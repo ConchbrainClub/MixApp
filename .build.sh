4bb7642ef598454c06252f6b842688f4ae52a476
lixinyang123 <lixinyangemil@outlook.com>
2024-09-28 23:35:02 +0800

    update build script

--- .build.sh
+++ .build.sh
@@ -3,7 +3,7 @@ mv ./node_modules/ ./MixApp.Web/wwwroot/lib
 
 wget https://dotnet.microsoft.com/download/dotnet/scripts/v1/dotnet-install.sh
 chmod +x ./dotnet-install.sh
-./dotnet-install.sh -v 8.0.401 --install-dir ~/cli
+./dotnet-install.sh -v 8.0.402 --install-dir ~/cli
 
 ~/cli/dotnet workload restore
 ~/cli/dotnet restore
